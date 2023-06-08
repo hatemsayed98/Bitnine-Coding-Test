4669e0f9217283a6a94f66fb2fb33325bc0b53a3
hatemsayed98 <hatemgad98@gmail.com>
2023-06-08 04:28:31 +0300

    Dynamic array in dynamic programming function

--- Fibonacci_three_ways/main.c
+++ Fibonacci_three_ways/main.c
@@ -3,20 +3,25 @@
 
 
 ///Iterative Method
-int itrFunction(int n) {
+int itrFunction(int n)
+{
     int a = 0, b = 1, c = 2, d;
 
-    if (n == 0) {
+    if (n == 0)
+    {
         return a;
     }
-    if (n == 1) {
+    if (n == 1)
+    {
         return b;
     }
-    if (n == 2) {
+    if (n == 2)
+    {
         return c;
     }
 
-    for (int i = 3; i <= n; i++) {
+    for (int i = 3; i <= n; i++)
+    {
         d = a + b;
         a = b;
         b = c;
@@ -27,33 +32,46 @@ int itrFunction(int n) {
 
 
 /// Recursive Method
-int recFunction(int n) {
-    if (n == 0) {
+int recFunction(int n)
+{
+    if (n == 0)
+    {
         return 0;
-    } else if (n == 1) {
+    }
+    else if (n == 1)
+    {
         return 1;
-    } else if (n == 2) {
+    }
+    else if (n == 2)
+    {
         return 2;
-    } else {
+    }
+    else
+    {
         return recFunction(n-3) + recFunction(n-2);
     }
 }
 
 ///Dynamic Programing Method
-int dpFunction(int n) {
-    int dp[n+1];
-
+int dpFunction(int n)
+{
+    int* dp;
+    dp = (int*)malloc(n * sizeof(int)+ 1);
     dp[0] = 0;
     dp[1] = 1;
     dp[2] = 2;
-    for (int i = 3; i <= n; i++) {
+    for (int i = 3; i <= n; i++)
+    {
         dp[i] = dp[i-3] + dp[i-2];
     }
-    return dp[n];
+    int result = dp[n];
+    free(dp);
+    return result;
 }
 
 
-int main() {
+int main()
+{
     int n = 25;
     int result = itrFunction(n);
     printf("F(%d) = %d\n", n, result);
